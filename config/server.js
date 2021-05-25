a5e8940619df78d95656a01a2df69bafa06abbe7
Henrique-Rossi <62766483+Henrique-Rossi@users.noreply.github.com>
2021-05-25 13:21:01 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4386144b03a801a6921fdc4336cd94ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df78b57e-c981-4962-b218-efac1eacae75'
+};
